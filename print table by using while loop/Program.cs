74c8ed0116974d7eb45d5631efc5f7a7380d80a9
akashkuchankar <kuchankarakash@gmail.com>
2022-10-04 10:43:36 -0700

    Add project files.

---
+++ print table by using while loop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_table_by_using_while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, M;
+            Console.WriteLine("Enter any munber");
+            N= Convert.ToInt32(Console.ReadLine());
+
+             M = 1;
+            while (M <= 10)   
+            {
+                Console.WriteLine("{0}*{1}={2}",N,M,(N*M));
+                M++;    
+               
+            }
+        }
+    }
+}
